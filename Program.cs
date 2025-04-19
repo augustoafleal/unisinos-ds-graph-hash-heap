fff93d4698e1e2602ba365092cc3b558c00f5c58
Augusto Leal <augustoafleal@gmail.com>
2025-04-19 14:32:54 -0300

    adjusts

--- Program.cs
+++ Program.cs
@@ -17,80 +17,28 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-                  string[] capitals =
+            UndirectedGraph graph = new UndirectedGraph(6);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(0, 5);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(2, 4);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(3, 5);
+
+            Console.WriteLine(graph.ToString());
+
+            DepthFirstSearch dfs = new DepthFirstSearch();
+            Vertex[] vertices = dfs.Execute(graph, 0);
+            foreach (var vertice in vertices)
             {
-                "AP - Macapá",
-                "RS - Porto Alegre",
-                "SC - Florianópolis",
-                "PR - Curitiba",
-                "SP - São Paulo",
-                "RJ - Rio de Janeiro",
-                "ES - Vitória",
-                "MG - Belo Horizonte",
-                "MS - Campo Grande",
-                "MT - Cuiabá",
-                "GO - Goiânia",
-                "DF - Brasília",
-                "BA - Salvador",
-                "SE - Aracaju",
-                "AL - Maceió",
-                "PE - Recife",
-                "PB - João Pessoa",
-                "RN - Natal",
-                "CE - Fortaleza",
-                "PI - Teresina",
-                "MA - São Luís",
-                "TO - Palmas",
-                "RO - Porto Velho",
-                "AC - Rio Branco",
-                "AM - Manaus",
-                "RR - Boa Vista",
-                "PA - Belém"
-            };
+                Console.WriteLine($"Vertice: {vertice.V} - Distance {vertice.Distance} - Time {vertice.FinishTime}");
+            }
 
-            string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent
-                .FullName;
-            string filePath = Path.Combine(projectDirectory, "GraphsShortestPath/graphCapitals.txt");
-
-            if (File.Exists(filePath))
+            foreach (var each in dfs.PathTo(5))
             {
-                string[] lines = File.ReadAllLines(filePath);
-
-                WeightedDirectedGraph graph = new WeightedDirectedGraph(int.Parse(lines[0]));
-
-                for (int i = 1; i < lines.Length; i++)
-                {
-                    string[] parts = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                    int v = int.Parse(parts[0]);
-                    int w = int.Parse(parts[1]);
-                    int weight = int.Parse(parts[2]);
-                    graph.AddEdge(new DirectedEdge(v, w, weight));
-                }
-
-                int source = 12;
-                Dijkstra dijkstra = new Dijkstra(graph, source);
-
-                Console.WriteLine($"> Listando caminhos e distâncias a partir da capital: {capitals[source]}");
-
-                for (int i = 0; i < graph.V; i++)
-                {
-                    int destination = i;
-                    foreach (var each in dijkstra.PathTo(destination))
-                    {
-                        Console.Write($"[{each} {capitals[each]}]");
-                        if (each != destination)
-                        {
-                            Console.Write(" -> ");
-                        }
-                        else
-                        {
-                            Console.Write(" ");
-                        }
-                    }
-
-                    Console.WriteLine($" | Distance: {dijkstra.Distance(destination)} km");
-                }
+                Console.Write(each + " ");
             }
         }
     }
